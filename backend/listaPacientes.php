223990f5b2a9d607b2ef2cd8a08b4d1012fa2090
Dunax <166877035+dunax10@users.noreply.github.com>
2024-08-22 18:39:55 -0300

    maquetacion
    Un poco de casos de uso y DER

---
+++ backend/listaPacientes.php
@@ -0,0 +1,32 @@
+<?php
+    include('conexion.php');
+    session_start();
+    if(isset($_SESSION['medico']))
+    {
+        if($_SESSION['medico'] == true)
+        {
+            $idUsuario = $_SESSION['idUsuario'];
+
+            $sql = "SELECT * FROM pacientes ORDER BY nombre ASC;";
+            //preparo la conexion
+            $stmt = $conn->prepare($sql);
+            //ejecuto la consulta
+            $stmt->execute();
+            //almaceno el resultado para verificar
+            $stmt->store_result();
+            //traigo los resultados de la consulta y la recorro con un while
+            $stmt->bind_result($idMedicina, $nombre);
+            while ($stmt->fetch()) 
+            {
+                echo "id $idMedicina nombre $nombre<br>";
+            }
+            $stmt->close();
+        }
+
+    }
+    else
+    {
+        $_SESSION['mensaje'] = "Necesitas iniciar sesion";
+        header('location: iniciarSesionFormulario.php');
+    }
+?>
