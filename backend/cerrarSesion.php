8a6ab436fc66d2a0be3ddae4b2416881e6b18792
Dunax <166877035+dunax10@users.noreply.github.com>
2024-11-08 01:05:55 -0300

    Dos boludeces
    Problemas con las url

--- backend/cerrarSesion.php
+++ backend/cerrarSesion.php
@@ -9,6 +9,6 @@ session_unset();
 session_destroy();
 
 // Redirige al usuario a la página de inicio de sesión u otra página
-header("Location: ../paginas/index.php");
+header("Location: ../index.php");
 exit();
 ?>
